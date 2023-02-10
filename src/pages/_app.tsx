ce7447acac0be3267d1cb1ddb8e4153355222877
katisd <katidd17@gmail.com>
2023-02-10 11:29:25 +0700

    add react query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.css";
 
+const queryClient = new QueryClient();
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
