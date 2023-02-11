34da435b21e1c5c4062d00e09173890b8fbc3bc2
katisd <katidd17@gmail.com>
2023-02-11 15:04:57 +0700

    🎨 art:chart color for each status

--- src/components/Charts/ChartComponents.tsx
+++ src/components/Charts/ChartComponents.tsx
@@ -1,4 +1,4 @@
-// chartjs import
+// import chartjs-adapter-date-fns
 import { Line } from "react-chartjs-2";
 import "chartjs-adapter-date-fns";
 import {
@@ -11,7 +11,7 @@ import {
   Tooltip,
   Legend,
   TimeScale,
-  ChartOptions,
+  type ChartOptions,
 } from "chart.js";
 ChartJS.register(
   CategoryScale,
@@ -25,7 +25,7 @@ ChartJS.register(
 );
 
 type ChartProps = {
-  chartData: { x: string; y: number }[] | undefined;
+  chartData: { x: string; y: number }[] | undefined | void;
   chartName: string;
   xAisUnit: "day" | "minute" | "hour";
 };
@@ -36,20 +36,30 @@ const ChartComponent: React.FC<ChartProps> = ({
   chartName,
   xAisUnit,
 }) => {
-  // map string to date and sort by date ascending
+  const helperColor = (gas: number) => {
+    if (gas > 1700) {
+      return "rgb(255, 255, 0,0.2)";
+    } else if (gas > 3000) {
+      return "rgb(255, 0, 0,0.2)";
+    } else {
+      return "rgb(4, 122, 255,0.2)";
+    }
+  };
+  // map string to date time and sort by date ascending
   const data = {
     labels: chartData
-      ?.map(({ x, y }) => ({ x: new Date(x), y }))
+      ?.map(({ x, y }) => ({ x: new Date(x).getTime(), y }))
       .sort((a, b) => (a.x > b.x ? 1 : -1)),
     chartName: chartName,
   };
+  const color = chartData?.map(({ x, y }) => helperColor(y));
   const Data = {
     labels: [],
     datasets: [
       {
         pointRadius: 8,
         hoverRadius: 10,
-        pointBackgroundColor: "rgb(4, 122, 255,0.2)",
+        pointBackgroundColor: color,
         data: data.labels,
         label: data.chartName,
       },
@@ -92,6 +102,7 @@ const ChartComponent: React.FC<ChartProps> = ({
   return chartData ? (
     <Line data={Data} options={options} width={400} height={200} />
   ) : (
+    // TODO Skeleton Loading for chart
     <></>
   );
 };
