9c688c832450ec7b816773e9f068bbd2018955ea
katisd <katidd17@gmail.com>
2023-02-11 15:06:20 +0700

    🗑️ clean up code * re fetch last command(multi device)

--- src/components/SideContent/Content.tsx
+++ src/components/SideContent/Content.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useGetCommand, useGetLastRecord } from "../common/useGetData";
 import DisplayGas from "./DisplayGas";
 import ForceOpenButton from "./ForceOpenButton";
 import WindowButton from "./WindowButton";
 
 const Content: React.FC = () => {
-  // TODO fetch gas status & gas volumn from API every 5 seconds
   const { data: lastRecordData, isLoading } = useGetLastRecord();
-  const { data } = useGetCommand();
   const [LocalCommand, setLocalCommand] = useState<boolean | undefined>(
     undefined
   );
-  const windowCommand = data?.isOpen;
+  const { data: serverCommandData } = useGetCommand(setLocalCommand);
+  const windowCommand = serverCommandData?.isOpen;
   return (
     <div className="order-1 flex flex-1 flex-col space-y-5 md:order-2">
       <DisplayGas
@@ -19,7 +18,6 @@ const Content: React.FC = () => {
         gasVolumn={lastRecordData?.gas_quantity}
         isLoading={isLoading}
       />
-      {/* when get data from api instead of static data this warning will lost */}
       {lastRecordData?.status != "DANGER" ? (
         <WindowButton
           LocalCommand={LocalCommand}
